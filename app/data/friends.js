49717404fa64cd44520c46ca7e85bae293f4a5cd
Aldalaeen <d.yousef71@gmail.com>
2019-05-29 20:33:16 -0700

    push
    push

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friendArr = [
+    {
+        
+        "photo" : "https://cdn.bulbagarden.net/upload/e/e2/133Eevee.png",
+        "name": "Eevee",
+        "scores" : [3,2,5,1,3,2,2,1,3,5]
+    },
+    {
+        
+        "image" : "https://nyoobserver.files.wordpress.com/2018/03/gettyimages-891922272.jpg?quality=80&w=970&strip",
+        "name": "Dwayne Johnson",
+        "scores" : [1,3,4,1,4,3,5,3,5,4]
+    },
+    {
+        
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8WuYFTgrxOGDhoxuItOX6Nu5-DeEKgcZAPTaLy83eh6tWozc",
+        "name": "Will Smith",
+        "scores" : [5,5,4,5,5,3,5,3,5,1]
+    },
+    {
+        
+        "photo" : "https://scstylecaster.files.wordpress.com/2014/11/x29298.jpg?w=399&h=600",
+        "name": "Kevin Costner",
+        "scores" : [3,2,5,1,3,2,2,1,3,5],
+    },
+    {
+        
+        "photo" : "https://amp.businessinsider.com/images/53a97800ecad04b76bf86499-750-562.jpg",
+        "name": "matthew mcconaughey",
+        "scores" : [2,1,2,4,5,5,2,3,1,4]
+    },
+    {
+        
+        "photo" : "https://imgix.ranker.com/user_node_img/64/1264415/original/james-stewart-photo-u19?w=650&q=50&fm=pjpg&fit=crop&crop=faces",
+        "name": "James Stewar",
+        "scores" : [4,1,3,5,4,2,5,2,1,3],
+    },
+    {
+        
+        "photo" : "http://content8.flixster.com/photo/10/10/05/10100536_gal.jpg",
+        "name": "BRAND THUNDER",
+        "scores" : [3,2,1,4,5,6,2,1,3,5],
+    },
+    {
+        
+        "photo" : "https://images.vogue.it/imgs/galleries/peole-are-talking-about/ossessione-del-giorno/006955/09-4573708_0x440.jpg",
+        "name": "Fan Bingbing",
+        "scores" : [1,2,4,3,2,5,4,5,1,2]
+    },
+    ];
+    
+// export to other modules
+    module.exports = friendArr;
---
+++ server.js
@@ -0,0 +1,32 @@
+//dependancies
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var apiRoutes = require('./app/routing/apiRoutes');
+var htmlRoutes = require('./app/routing/htmlRoutes');
+
+var PORT = process.env.PORT||3000;
+
+//General Routes
+
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static(__dirname + '/app/public'));
+// app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+// //make sure app can parse data
+
+//error logs
+// app.use(function(req, res, next){
+//     var err = new Error("Something is wrong");
+//     err.status= 404;
+//     next(err);
+// });
+
+//listening
+app.listen(PORT, function(){
+    console.log("App listening in on PORT" + PORT)
+})
